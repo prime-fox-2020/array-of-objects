65a49f35cf22d419f779c0253b565e0a46c84aa8
aireljordan98 <joeyjordanwilliams@gmail.com>
2020-04-18 15:28:32 +0700

    Done-additional

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ createObj('Icha', 1, 'female')
 createObj('Adhit', 2, 'male')
 createObj('Tama', 2, 'male')
 createObj('Rifky', 3, 'male')
-
+console.log(arr)
 
 function getData(name){
   for(let i = 0; i < arr.length; i++){
